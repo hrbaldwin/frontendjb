69882b919efb413a3c837c83d60bb134229c1a73
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-11-01 13:59:05 -0500

    oops full push

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const Posts = (props) => {
   const userPosts = props.userPosts;
   const setUserPosts = props.setUserPosts;
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
 };
 
 export default Posts;
