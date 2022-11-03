af82d3b4d6926dc6edf6fb823c725db1f46ae950
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-11-02 21:43:48 -0500

    added some more styling for the time being-- to createpost and login

--- src/components/CreatePosts.jsx
+++ src/components/CreatePosts.jsx
@@ -6,30 +6,30 @@ const CreatePosts = (props) => {
   const { userPosts, setUserPosts } = props;
 
   async function handleSubmit(e) {
-    console.log(e)
+    console.log(e);
     e.preventDefault();
     const title = e.target[0].value;
     const content = e.target[1].value;
     const tags = e.target[2].value;
-    console.log(title, content, tags)
+    console.log(title, content, tags);
     const token = localStorage.getItem("token");
     const newPost = await createPost(token, title, content, tags);
     console.log("this is newPost console log", newPost);
   }
   return (
     <>
-      <div>
-        <h3>Create Post</h3>
-        <form onSubmit={handleSubmit}>
+      <div className="formBoxDiv">
+        <h3 className="headerThree">share your dream</h3>
+        <form onSubmit={handleSubmit} className="formBox">
           <label>Title:</label>
           <input id="title" type="text" required />
           <label>Content:</label>
           <input id="content" type="text" required />
           <label>Hashtags:</label>
           <input id="tags" type="text" required />
-          <button>Submit</button>
+          <button className="submitButton">submit</button>
           <Link to="/">
-            <button>Go back</button>
+            <button className="backButton">←</button>
           </Link>
         </form>
       </div>
